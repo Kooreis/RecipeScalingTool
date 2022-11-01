795415acf30e285b53b56578993e92fe10d18823
Kooreis <kooreis.official@gmail.com>
2022-11-01 06:00:00 -0500

    In this update, the Scanner class is imported and the RecipeScalingTool class is created. The main method is also created, and the user is prompted to enter the original number of servings and the desired number of servings.

--- Recipe_Scaling_Tool.java
+++ Recipe_Scaling_Tool.java
@@ -1,4 +1,3 @@
-```java
 import java.util.Scanner;
 
 public class RecipeScalingTool {
@@ -8,21 +7,3 @@ public class RecipeScalingTool {
         int originalServings = scanner.nextInt();
         System.out.println("Enter the desired number of servings: ");
         int desiredServings = scanner.nextInt();
-        System.out.println("Enter the number of ingredients: ");
-        int numIngredients = scanner.nextInt();
-        String[] ingredients = new String[numIngredients];
-        double[] quantities = new double[numIngredients];
-        for (int i = 0; i < numIngredients; i++) {
-            System.out.println("Enter ingredient " + (i + 1) + " name: ");
-            ingredients[i] = scanner.next();
-            System.out.println("Enter ingredient " + (i + 1) + " quantity: ");
-            quantities[i] = scanner.nextDouble();
-        }
-        System.out.println("Scaled Recipe: ");
-        for (int i = 0; i < numIngredients; i++) {
-            double scaledQuantity = quantities[i] * desiredServings / originalServings;
-            System.out.println(ingredients[i] + ": " + scaledQuantity);
-        }
-    }
-}
-```
