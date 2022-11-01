7bbba49cce074dfaf4fb95cb35830d788f5bf328
Kooreis <kooreis.official@gmail.com>
2022-11-01 06:00:00 -0500

    Added a prompt for the user to enter the original number of servings and stored the input in a variable.

--- Recipe_Scaling_Tool.cs
+++ Recipe_Scaling_Tool.cs
@@ -1,4 +1,3 @@
-```CSharp
 using System;
 
 class Program
@@ -7,25 +6,5 @@ class Program
     {
         Console.WriteLine("Enter the original number of servings: ");
         int originalServings = Convert.ToInt32(Console.ReadLine());
-
-        Console.WriteLine("Enter the desired number of servings: ");
-        int desiredServings = Convert.ToInt32(Console.ReadLine());
-
-        Console.WriteLine("Enter the number of ingredients: ");
-        int numIngredients = Convert.ToInt32(Console.ReadLine());
-
-        for (int i = 0; i < numIngredients; i++)
-        {
-            Console.WriteLine("Enter the quantity of ingredient " + (i + 1) + ": ");
-            double quantity = Convert.ToDouble(Console.ReadLine());
-
-            double newQuantity = (quantity / originalServings) * desiredServings;
-
-            Console.WriteLine("The new quantity for ingredient " + (i + 1) + " is: " + newQuantity);
-        }
-
-        Console.WriteLine("Press any key to exit...");
-        Console.ReadKey();
     }
 }
-```
