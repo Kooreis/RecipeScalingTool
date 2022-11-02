6e8502fc14cabcc8e8effd113b4f2b53eea5749b
Kooreis <kooreis.official@gmail.com>
2022-11-02 05:00:00 -0500

    Added a prompt for the user to enter the desired number of servings and stored the input in a variable.

--- Recipe_Scaling_Tool.cs
+++ Recipe_Scaling_Tool.cs
@@ -6,5 +6,8 @@ class Program
     {
         Console.WriteLine("Enter the original number of servings: ");
         int originalServings = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter the desired number of servings: ");
+        int desiredServings = Convert.ToInt32(Console.ReadLine());
     }
 }
