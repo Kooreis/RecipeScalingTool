2185fb6bfeed1ebdef781e2377ca3ef3fa26d61e
Kooreis <kooreis.official@gmail.com>
2022-11-02 05:00:00 -0500

    In this update, the user is prompted to enter the number of ingredients. Two arrays are created to store the names of the ingredients and their quantities.

--- Recipe_Scaling_Tool.java
+++ Recipe_Scaling_Tool.java
@@ -1,9 +1,4 @@
-import java.util.Scanner;
-
-public class RecipeScalingTool {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the original number of servings: ");
-        int originalServings = scanner.nextInt();
-        System.out.println("Enter the desired number of servings: ");
-        int desiredServings = scanner.nextInt();
+System.out.println("Enter the number of ingredients: ");
+        int numIngredients = scanner.nextInt();
+        String[] ingredients = new String[numIngredients];
+        double[] quantities = new double[numIngredients];
